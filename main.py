f54d0b4465bd86d5673e4c5e9b61bc0f6e9f572c
Chirpatel <chirpatelss@gmail.com>
2020-05-05 18:10:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+os.system('pip install --upgrade pip')
+#os.system('pip install tensorflow==2.2.0rc4')
+
+import random, string
+from flask import Flask, render_template, request, jsonify
+import base64
+import numpy as np
+import io
+from PIL import Image
+from base64 import decodestring
+#import tensorflow as tf
+from keras import backend as K
+from flask import request
+from flask import jsonify
+from flask import Flask
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from skimage.io import imread
+from skimage.transform import resize
+import shutil
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+ok_chars = string.ascii_letters + string.digits
+
+
+@app.route('/')  # What happens when the user visits the site
+def base_page():
+	return render_template(
+		'index.html',  # Template file path, starting from the templates folder. 
+	)
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+model=0
+def get_model():
+    global model
+    model = keras.models.load_model("keras_mnist.h5")
+    print(" * Model Loded!")
+
+print(" * Loading Keras Model...")
+get_model()
+
+@app.route("/",methods=["POST"])
+def predict():
+    CATEGORIES=["NORMAL","CNV","DME","DRUSEN"]
+    message = request.get_json(force=True)
+    print(message)
+    
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    bytesio=io.BytesIO(decoded)
+    filename="test.png"
+    with open(filename, "wb") as outfile:
+        outfile.write(bytesio.getbuffer())
+    num=find()
+    response = {
+        'prediction': str(num)
+    }
+    return jsonify(response)
+
+def find():
+	CATEGORIES=["NORMAL","CNV","DME","DRUSEN"]
+	test=(np.array(resize(imread("test.png"),(28,28,1)))).reshape(1,784)
+	prediction = model.predict(test)
+	num=np.argmax(prediction[0])
+	print(num)
+	return  num
+
+
+
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=5000,  # Randomly select the port the machine hosts on.
+		threaded=False
+	)
---
+++ static/canvas.js
@@ -0,0 +1,45 @@
+window.addEventListener('load',()=>{
+    const cnvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext('2d');
+
+    canvas.height = 280;
+    canvas.width  = 280;
+
+    let painting =false;
+    function startPosition(e){
+        painting=true;
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+    function draw(e){
+        if(!painting) return;
+        ctx.lineWidth=10;
+        ctx.lineCap - "round";
+        ctx.lineTo(e.clientX,e.clientY);
+        ctx.stroke();
+        /*ctx.beginPath();
+        ctx.moveTo(e.clientX,e.clientY);*/
+    }
+    canvas.addEventListener("mousedown",startPosition);
+    canvas.addEventListener("mouseup",finishedPosition);
+    canvas.addEventListener("mousemove",draw);
+    document.getElementById("Submit").addEventListener("click",predict);
+    function predict(){
+        document.getElementById("downloader").download = "image.png";
+        document.getElementById("downloader").href = document.getElementById("canvas").toDataURL()
+						let message = {
+								image: document.getElementById("canvas").toDataURL().replace("data:image/png;base64,","")
+						}
+						console.log(message);
+
+						$.post("https://number-prediction.chir0313.repl.co/",JSON.stringify(message), function(response){
+								$("#d-prediction").text(response.prediction);
+								console.log(response);
+						});
+
+    }
+
+})
